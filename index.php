fee40db73c5a009161595fe2a754d61b72fe0541
iidashzii5nk <iifranciscoii5nk@gmail.com>
2022-06-22 09:13:48 -0500

    Menu respons deplegable con HTML CSS JQUERY

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="estyle.css">
+    <title>Document</title>
+</head>
+<body>
+
+			<span class="nav-bar" id="btnMenu"><i class="fa-solid fa-align-justify"></i>Menu</span>
+
+			<nav class="main-nav">
+				<ul class="menu" id="menu">
+				
+					<li class="menu_item container-submenu"><a href="#" class="menu__link submenu-btn">Empleados<i class="fa-solid fa-chevron-down"></i></a>
+						<ul class="submenu">
+							<li class=""><a href="#" class="menu__link">Juan</a></li>
+						</ul>
+					</li>
+					<li class="menu_item"><a href="#" class="menu__link">CARRITO</a></li>
+					<li class="menu_item"><a href="#" class="menu__link">iniciar sesion</a></li>
+					<li class="menu_item"><a href="#" class="menu__link">Cerrar sesion</a></li>
+				</ul>
+			</nav>
+
+
+
+        <script src="menu.js"></script>
+</body>
+</html>
